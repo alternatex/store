ae6d1bd0f2a11c941fdd2296ec0724de543714a9
alternatex <gianni.furger@gmail.com>
2013-12-07 20:33:32 +0100

    updated api docs

--- docs/api.js
+++ docs/api.js
@@ -1,38 +1,17 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "CommonJs",
-        "Csv",
-        "Dropbox",
-        "File",
-        "FileSystem",
-        "Format",
-        "Item",
-        "Json",
-        "Markdown",
-        "Memory",
-        "Object",
-        "Repository",
-        "Resource",
-        "Schema",
         "Store",
-        "Store (Client)",
-        "Store (Server)"
+        "Store (Client)"
     ],
     "modules": [
-        "Client",
-        "Server"
+        "Client"
     ],
     "allModules": [
         {
             "displayName": "Client",
             "name": "Client",
             "description": "A lightweight datastore wrapper"
-        },
-        {
-            "displayName": "Server",
-            "name": "Server",
-            "description": "Json Formatted Contents"
         }
     ]
 } };
