b6e54739c35f16afcff472038f1efeed6c3ebaf9
alternatex <gianni.furger@gmail.com>
2014-01-05 06:03:29 +0100

    updated resources references

--- examples/js/app.js
+++ examples/js/app.js
@@ -3,9 +3,9 @@ requirejs.config({
     "deps": ["jquery", "underscore", "store"],
     "paths": {
       "app": "../app",
-      "store": "../../../src/client/store",
+      "store": "../store",
       "configuration": "../configuration",
-      "underscore": "../../../vendor/underscore.deferred",
+      "underscore": "../underscore.deferred",
       "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
       "data": "//localhost/store/examples/data/data.php",
       "shop": "//localhost/store/examples/data/shop.php"
