91e0c214d5251b9eae65d03599308574967ee186
alternatex <gianni.furger@gmail.com>
2014-01-21 20:48:01 +0100

    removed develop crumbs from master. timetravel.

--- examples/js/app.js
+++ examples/js/app.js
@@ -3,7 +3,7 @@ requirejs.config({
     "deps": ["jquery", "underscore", "store"],
     "paths": {
       "app": "../app",
-      "store": "../store",
+      "store": "../../../src/client/store",
       "configuration": "../configuration",
       "underscore": "../underscore.deferred",
       "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
